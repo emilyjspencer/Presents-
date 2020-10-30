34eaab4a85d93ec2e4499ec2e4cff6dd92dab5d3
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-10-30 13:47:09 +0000

    Adds first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+
+  let wrapper;
+
+  beforeEach(() => wrapper = shallow(<App />));
+
+  it('renders a <div />', () => {
+    expect(wrapper.find('div').length).toEqual(1);
+  })
 });
+
+
